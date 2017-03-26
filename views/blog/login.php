0b2ecf8a098086429d5838cd817581902fcbe1c1
Cruelkid <akosovich@narod.ru>
2017-03-26 15:19:11 +0300

    stylizing

--- views/blog/login.php
+++ views/blog/login.php
@@ -1,12 +1,24 @@
 <?php 
+	use yii\helpers\Html;
 	use yii\bootstrap\ActiveForm;
+
+	$this->title = "Login";
 ?>
-<?php $form = ActiveForm::begin(); ?>
+<div class="site-login">
+	<h1><?= Html::encode($this->title) ?></h1>
+	<p>Please fill out these fields to login:</p>
+	<div class="row">
+		<div class="col-md-5 col-sm-12">
+			<?php $form = ActiveForm::begin(); ?>
 
-	<?= $form->field($loginForm, 'email')->textInput(); ?>
-	<?= $form->field($loginForm, 'password')->passwordInput(); ?>
-	<?= $form->field($loginForm, 'rememberMe')->checkbox(); ?>
+				<?= $form->field($loginForm, 'email')->textInput(['autofocus' => true]); ?>
+				<?= $form->field($loginForm, 'password')->passwordInput(); ?>
+				<?= $form->field($loginForm, 'rememberMe')->checkbox(); ?>
 
-	<input type="submit" name="login" value="Login">
-
-<?php ActiveForm::end(); ?>
+				<div class="form-group">
+					<?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+				</div>	
+			<?php ActiveForm::end(); ?>
+		</div>
+	</div>		
+</div>
--- views/blog/signup.php
+++ views/blog/signup.php
@@ -1,17 +1,15 @@
 <?php
-
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
 $this->title = 'Signup';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <div class="site-signup">
 	<h1><?= Html::encode($this->title) ?></h1>
 	<p>Please fill out these fields to signup:</p>
 	<div class="row">
-		<div class="col-lg-5">
+		<div class="col-md-5 col-sm-12">
 
 			<?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
 				<?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
